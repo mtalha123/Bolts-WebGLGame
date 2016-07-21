4457bedcc398b39682adca76a3e2d0fb49ac1b9e
Muhammad Talha <muhtalha132@hotmail.com>
2016-07-21 15:43:07 -0800

    Create physics bodies for Box2D in the BorderLightning module and add appropriate get methods for each of the border sides (left, top, right, bottom)

--- BorderLightning.js
+++ BorderLightning.js
@@ -1,4 +1,4 @@
-define(['LightningPiece'], function(LightningPiece){
+define(['LightningPiece', 'Box2DStuff'], function(LightningPiece, Box2DStuff){
     
     var canvasWidth, canvasHeight, borderLightningPiece;
     
@@ -6,6 +6,8 @@ define(['LightningPiece'], function(LightningPiece){
     
     var widthOfBlueThing;
     var heightOfBlueThing;
+    
+    var leftSide, topSide, rightSide, bottomSide;
     
     function initialize(p_canvasWidth, p_canvasHeight){
         canvasWidth = p_canvasWidth;
@@ -23,7 +25,50 @@ define(['LightningPiece'], function(LightningPiece){
                             margin,                margin ]       
         ];
         
-        borderLightningPiece = new LightningPiece(canvasWidth, canvasHeight, borderPath, 20, 20, {closePath: true});   
+        borderLightningPiece = new LightningPiece(canvasWidth, canvasHeight, borderPath, 20, 20, {closePath: true});
+        
+        var physicsBodyPositions = [ 
+                                     [(margin + (widthOfBlueThing/2)) * Box2DStuff.scale, (margin + (heightOfBlueThing/2)) * Box2DStuff.scale],  
+                                     [(canvasWidth - margin) * Box2DStuff.scale, (margin + (widthOfBlueThing/2)) * Box2DStuff.scale],
+                                     [(margin + (widthOfBlueThing/2)) * Box2DStuff.scale, (canvasHeight - margin) * Box2DStuff.scale],
+                                     [(margin + (heightOfBlueThing/2)) * Box2DStuff.scale, (margin + (widthOfBlueThing/2)) * Box2DStuff.scale]                 
+                                   ]
+        
+        
+        var bodyDefs = new Box2DStuff.b2BodyDef();     
+        var fixtureDefs = new Box2DStuff.b2FixtureDef();
+        
+        //some of the following lines are settings that will apply to all the sides and the following lines also create the top side 
+        bodyDefs.type = Box2DStuff.b2Body.b2_staticBody;
+        bodyDefs.position.Set(physicsBodyPositions[0][0], physicsBodyPositions[0][1]);
+        fixtureDefs.density = 1;
+        fixtureDefs.friction = 0;
+        fixtureDefs.resitituation = 1;
+        fixtureDefs.shape = new Box2DStuff.b2PolygonShape.SetAsBox((widthOfBlueThing/2) * Box2DStuff.scale, (heightOfBlueThing/2) * Box2DStuff.scale);
+        topSide = Box2DStuff.physicsWorld.CreateBody(bodyDefs);
+        topSide.CreateFixture(fixtureDefs);
+        topSide.SetUserData("top");
+        
+        //since the bottom side is the exact same as the top one except for the position, the bottom is set after the top instead of going clockwise (so instead of the right side in this case)
+        bodyDefs.position.Set(physicsBodyPositions[2][0], physicsBodyPositions[2][1]);
+        bottomSide = Box2DStuff.physicsWorld.CreateBody(bodyDefs);
+        bottomSide.CreateFixture(fixtureDefs);
+        bottomSide.SetUserData("bottom");
+        
+        //left side
+        bodyDefs.position.Set(physicsBodyPositions[3][0], physicsBodyPositions[3][1]);
+        fixtureDefs.shape = new Box2DStuff.b2PolygonShape.SetAsBox((heightOfBlueThing/2) * Box2DStuff.scale, (widthOfBlueThing/2) * Box2DStuff.scale)
+        leftSide = Box2DStuff.physicsWorld.CreateBody(bodyDefs);
+        leftSide.CreateFixture(fixtureDefs);
+        leftSide.SetUserData("left");
+        
+        
+        //just like the bottom side was set right after the top, the right side if set after the left because it hsa much of the same settings except for the positioning as well as the user data
+        bodyDefs.position.Set(physicsBodyPositions[1][0], physicsBodyPositions[1][1]);
+        rightSide = Box2DStuff.physicsWorld.CreateBody(bodyDefs);
+        rightSide.CreateFixture(fixtureDefs);
+        rightSide.SetUserData("right");        
+        
     }
     
     function draw(context, interpolation){
@@ -48,6 +93,20 @@ define(['LightningPiece'], function(LightningPiece){
         return canvasHeight - margin;
     }
     
+    function getLeftSidePhysicsBody(){
+        return leftSide;
+    }
+    function getTopSidePhysicsBody(){
+        return topSide;
+    }
+    function getRightSidePhysicsBody(){
+        return rightSide;
+    }
+    function getBottomSidePhysicsBody(){
+        return bottomSide;
+    }
+    
+    
    
     
     return {
@@ -57,6 +116,10 @@ define(['LightningPiece'], function(LightningPiece){
         getLeftX: getLeftX,
         getTopY: getTopY,
         getRightX: getRightX,
-        getBottomY: getBottomY
+        getBottomY: getBottomY,
+        getLeftSidePhysicsBody: getLeftSidePhysicsBody,
+        getTopSidePhysicsBody: getTopSidePhysicsBody,
+        getRightSidePhysicsBody: getRightSidePhysicsBody,
+        getBottomSidePhysicsBody: getBottomSidePhysicsBody,
     }
 });
