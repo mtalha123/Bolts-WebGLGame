00a4adc5c7501d760beaa01c30e7cf0f3bf5f1c3
Muhammad Talha <muhtalha132@hotmail.com>
2016-08-13 16:50:29 -0800

    Add new custom utility module - isObjectEmpty.

---
+++ Custom Utility/isObjectEmpty.js
@@ -0,0 +1,12 @@
+define([], function(){
+    function isEmpty(obj) {
+        for(var prop in obj) {
+            if(obj.hasOwnProperty(prop))
+                return false;
+        }
+
+        return true;
+    }
+    
+    return isEmpty;
+});
