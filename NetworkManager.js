8a3d2a2ad59451536f29c93a6b0b155e28391b8c
Muhammad Talha <muhtalha132@hotmail.com>
2016-08-28 17:54:57 -0800

    Make client connect to proper ip address because this computer doesn't have a static ip address so it keeps changing.

--- NetworkManager.js
+++ NetworkManager.js
@@ -6,7 +6,7 @@ define(['socketio', 'EventSystem', 'Custom Utility/Timer'], function(io, EventSy
     var socket;
     
     function initialize(canvasWidth, canvasHeight, listenerFunction){
-        socket = io.connect('192.168.0.19:4000');
+        socket = io.connect('192.168.0.18:4000');
  
         socket.on("connect", function(data){
             console.log("Connected to server.");
