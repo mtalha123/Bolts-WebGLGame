82bd92456ef3e061e4d0f0fae057703d770cea2b
Muhammad Talha <muhtalha132@hotmail.com>
2017-04-11 01:24:32 -0800

    Fix aspect ratio issues in lightningFragmentShader.

--- Shaders/lightningFragmentShader.glsl
+++ Shaders/lightningFragmentShader.glsl
@@ -13,7 +13,7 @@ uniform vec3 glowColor;
 uniform sampler2D noise;
 uniform sampler2D coords;
 
-float t_fluctuation;
+
 
 float improvedSin(float value){
 	return (sin(value) + 1.0) / 2.0;
@@ -31,10 +31,11 @@ float transformToAspectRatioMeasurement(float aspectRatio, float baseMeasurement
     return baseMeasurement + ( cos(abs(uvAngle)) * (aspectRatio - 1.0) * baseMeasurement );
 }
 
-void main()
-{
+void main(){
 	vec2 currentUV = gl_FragCoord.xy / iResolution.xy; 
     float aspectRatio = iResolution.x / iResolution.y;
+    
+    currentUV.x *= aspectRatio;
         
     float lightningAmount = 0.025 * iResolution.x;
     float lineWidthUV, lineWidthUV_t; 
@@ -56,13 +57,16 @@ void main()
     float b = 0.0;
     for(int i = 0; i < 6-1; i++){
         t_lightningStartUV = texture2D(coords, vec2(b * (1.0/8.0), 0.5) ).xy;
-        t_lightningEndUV = texture2D(coords, vec2((b+1.0) * (1.0/8.0), 0.5) ).xy;  
+        t_lightningEndUV = texture2D(coords, vec2((b+1.0) * (1.0/8.0), 0.5) ).xy;
         
         if(t_lightningStartUV.x > t_lightningEndUV.x){
         	float temp = t_lightningStartUV.x;
             t_lightningStartUV.x = t_lightningEndUV.x;
             t_lightningEndUV.x = temp;
         }
+
+        t_lightningStartUV.x *= aspectRatio;
+        t_lightningEndUV.x *= aspectRatio;
         
         t_lengthOfLightning = distance(t_lightningStartUV, t_lightningEndUV);
         
@@ -70,17 +74,15 @@ void main()
         angle *= (-1.0);
         mat2 rotationMatrix = mat2(cos(angle), -sin(angle), sin(angle), cos(angle));
         
-        lineWidthUV_t = transformToAspectRatioMeasurement(aspectRatio, lineWidth / iResolution.y, angle);
-        
-        t_fluctuation = fluctuation + (fluctuation *  cos(abs(angle)) * (aspectRatio - 1.0));
+        lineWidthUV_t = lineWidth / iResolution.y;//transformToAspectRatioMeasurement(aspectRatio, lineWidth / iResolution.y, angle);
         
         t_lightningStartUV_t = t_lightningStartUV * rotationMatrix;
         t_currentUV_t = (currentUV - t_lightningStartUV) * rotationMatrix;
         
         t_xClamped = clamp(t_currentUV_t.x, 0.0, t_lengthOfLightning);
-        float pureNoiseVal = texture2D(noise, (vec2(t_xClamped, iGlobalTime / 1024.0))).r;
+        float pureNoiseVal = texture2D(noise, (vec2(clamp(t_currentUV_t.x / aspectRatio, 0.0, t_lengthOfLightning), iGlobalTime / 1024.0))).r;
         pureNoiseVal = map(0.0, 1.0, -1.0, 1.0, pureNoiseVal);
-    	t_yNoiseVal = pureNoiseVal * ( (t_fluctuation * lightningCos(t_xClamped, t_lengthOfLightning)) / iResolution.x);
+    	t_yNoiseVal = pureNoiseVal * ( (fluctuation * lightningCos(t_xClamped, t_lengthOfLightning)) / iResolution.x);
     	t_pointOnLightning = vec2(t_xClamped, clamp(t_currentUV_t.y, t_yNoiseVal - lineWidthUV_t, t_yNoiseVal + lineWidthUV_t)); 
     	testDistance = distance(t_currentUV_t, t_pointOnLightning);
         
@@ -100,7 +102,7 @@ void main()
     }
     
     float invertedDistance = clamp(1.0 / distanceToPoint, 1.0, 300.0);
-    float multiplier = invertedDistance * (1.0 / 300.0);// * (glowFactor / iResolution.x);
+    float multiplier = invertedDistance * (glowFactor / iResolution.x);
     
     if( (currentUV_t.y >= (yNoiseVal - lineWidthUV)) && (currentUV_t.y <= (yNoiseVal + lineWidthUV)) ){
         finalColor = multiplier * glowColor;
@@ -142,7 +144,7 @@ void main()
     }
     
     //-------------------------------
-   float alpha = 1.0 - smoothstep(0.001, 0.015, distanceToPoint);
+   float alpha = 1.0 - smoothstep(0.001, 0.02, distanceToPoint);
    finalColor = vec3(1.0, 1.0, 0.0);
    if(alpha < 1.0){
         finalColor = vec3(1.0, 1.0, 0.9);
