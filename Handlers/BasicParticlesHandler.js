53b212cb5088b8340501741ec0368b2aedfe1db4
Muhammad Talha <muhtalha132@hotmail.com>
2017-08-11 00:46:45 -0800

    Add particle effect shaders and handler.

---
+++ Handlers/BasicParticlesHandler.js
@@ -0,0 +1,104 @@
+define(['Handlers/Handler', 'Custom Utility/getVerticesUnNormalized', 'Custom Utility/getGLCoordsFromNormalizedShaderCoords', 'ParticleEngine', 'Custom Utility/Timer'], function(Handler, getVerticesUnNormalized, getGLCoordsFromNormalizedShaderCoords, ParticleEngine, Timer){
+    
+    function BasicParticlesHandler(shouldDraw, numParticles, canvasWidth, canvasHeight, gl, zOrder, x, y, ShaderLibrary){
+        Handler.call(this, shouldDraw, 0, 0, zOrder, canvasWidth, canvasHeight);
+        
+        this._shaderProgram = ShaderLibrary.requestProgram(ShaderLibrary.PARTICLE);
+        this._numParticles = numParticles;
+        this._time = 1;
+        
+        this._uniforms = {
+            iResolution: {
+                type: "vec2",
+                value: [canvasWidth, canvasHeight]
+            },
+            particleFX: {
+                type: "float",
+                value: [1.0]
+            },
+            iGlobalTime: {
+                type: "float",
+                value: [1.0]
+            },
+            center: {
+                type: "vec2",
+                value: [800, 500]
+            },
+            radius: {
+                type: "float",
+                value: [50]
+            },
+            maxLifetime: {
+                type: "float",
+                value: [50]
+            },
+            velocityMagnitude: {
+                type: "float",
+                value: [3]
+            },
+            accelerationMagnitude: {
+                type: "float",
+                value: [0.0]
+            },
+            particlesColor: {
+                type: "vec3",
+                value: [1.0, 1.0, 1.0]
+            }
+        };       
+        
+        var randVals = [];
+        for(var i = 0; i < numParticles; i++){
+            var randomVal = Math.random();
+            for(var a = 0; a < 24; a++){
+                randVals.push(randomVal);
+            }
+        }        
+        this._attributes.randVals = randVals;
+        
+        this.setPosition(x, y);
+    }   
+    
+    //inherit from Handler
+    BasicParticlesHandler.prototype = Object.create(Handler.prototype);
+    BasicParticlesHandler.prototype.constructor = BasicParticlesHandler;
+    
+    
+    BasicParticlesHandler.prototype.update = function(){
+        if(this._time <= this._uniforms.maxLifetime.value[0]){
+            this._time++;
+            this._uniforms.iGlobalTime.value[0] = this._time;
+        }else{
+            this._shouldDraw = false;
+        }   
+    }
+    
+    BasicParticlesHandler.prototype.doEffect = function(){
+        this._time = 1;
+        this._shouldDraw = true;
+    }
+    
+    
+    BasicParticlesHandler.prototype.setPosition = function(newX, newY){
+        this._uniforms.center.value[0] = newX + this._uniforms.radius.value[0];
+        this._uniforms.center.value[1] = newY - this._uniforms.radius.value[0];
+        this._generateVerticesFromCurrentState();
+    }
+    
+    BasicParticlesHandler.prototype.setParticlesColor = function(r, g, b){
+        this._uniforms.particlesColor.value = [r, g, b];
+    }
+
+    
+    BasicParticlesHandler.prototype._generateVerticesFromCurrentState = function(){        
+        var radius_t = this._uniforms.radius.value[0] * 1.5;
+        var centerX = this._uniforms.center.value[0];
+        var centerY = this._uniforms.center.value[1];
+        
+        this._attributes.vertexPosition = [];
+        for(var i = 0; i < this._numParticles; i++){
+            this._attributes.vertexPosition = this._attributes.vertexPosition.concat( getVerticesUnNormalized(centerX - radius_t, centerY - radius_t, radius_t * 2, radius_t * 2, this._canvasWidth, this._canvasHeight) );
+        }
+    }
+
+    return BasicParticlesHandler;
+});
