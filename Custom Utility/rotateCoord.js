94afd416fff3c640577916607c9b2d14e58fb176
Muhammad Talha <muhtalha132@hotmail.com>
2017-08-17 23:03:17 -0800

    Add new Custom Utility module; rotateCoord.

---
+++ Custom Utility/rotateCoord.js
@@ -0,0 +1,12 @@
+define(['Custom Utility/Vector'], function(Vector){
+    function rotateCoord(point, angle, center){
+        var pointRelToOrigin = center.subtractFrom(point);
+        var rotatedCoordX = (pointRelToOrigin.getX() * Math.cos(angle)) - (pointRelToOrigin.getY() * Math.sin(angle));
+        var rotatedCoordY = (pointRelToOrigin.getX() * Math.sin(angle)) + (pointRelToOrigin.getY() * Math.cos(angle));
+        var rotatedCoord = new Vector(rotatedCoordX, rotatedCoordY);
+        
+        return center.addTo(rotatedCoord);
+    }
+    
+    return rotateCoord;
+});
