1d88c9d7b2dd5d4e51678380634d5f84fe732864
Muhammad Talha <muhtalha132@hotmail.com>
2017-08-17 23:02:29 -0800

    Add rotation feature into CircularHitRegions. Also add labeling feature to CircularHitBox.

--- Custom Utility/CircularHitRegions.js
+++ Custom Utility/CircularHitRegions.js
@@ -1,8 +1,10 @@
-define(['Custom Utility/distance'], function(distance){
-    function CircularHitBox(centerX, centerY, radius){
+define(['Custom Utility/distance', 'Custom Utility/rotateCoord', 'Custom Utility/Vector'], function(distance, rotateCoord, Vector){
+    function CircularHitBox(centerX, centerY, radius, label){
         this._centerX = centerX;
         this._centerY = centerY;
         this._radius = radius;
+        this._label = label;
+        this.activated = true;
     }
     
     CircularHitBox.prototype.isInRegion = function(checkX, checkY){
@@ -24,20 +26,27 @@ define(['Custom Utility/distance'], function(distance){
         return [x, y];
     }
     
+    CircularHitBox.prototype.getLabel = function(){
+        return this._label;
+    }
+    
     function CircularHitRegions(centerXOfAllRegions, centerYOfAllRegions){
         this._centerX = centerXOfAllRegions;
         this._centerY = centerYOfAllRegions;
         this._regions = [];
+        this._labelCounter = 1;
     }
     
     CircularHitRegions.prototype.addRegion = function(centerX, centerY, radius){
-        var hitbox = new CircularHitBox(centerX, centerY, radius);
+        var hitbox = new CircularHitBox(centerX, centerY, radius, this._labelCounter);
         this._regions.push(hitbox);
+        
+        this._labelCounter++;
     }
     
     CircularHitRegions.prototype.isInAnyRegion = function(checkX, checkY){
         for(var i = 0; i < this._regions.length; i++){
-            if(this._regions[i].isInRegion(checkX, checkY)){
+            if(this._regions[i].activated && this._regions[i].isInRegion(checkX, checkY)){
                 return this._regions[i];
             }
         }
@@ -61,6 +70,27 @@ define(['Custom Utility/distance'], function(distance){
     CircularHitRegions.prototype.getPosition = function(){
         return [this._centerX, this._centerY];
     }
+    
+    CircularHitRegions.prototype.getRegions = function(){
+        return this._regions;
+    }
+    
+    CircularHitRegions.prototype.rotateAllRegions = function(angle){
+        for(var i = 0; i < this._regions.length; i++){
+            var currRegion = this._regions[i];
+            var currPos = currRegion.getPosition();
+            var newPos = rotateCoord(new Vector(currPos[0], currPos[1]), angle, new Vector(this._centerX, this._centerY));
+            currRegion.setPosition(newPos.getX(), newPos.getY());
+        }
+    }
+    
+    CircularHitRegions.prototype.activateAllRegions = function(){
+        this._regions = this._regions.map(function(currValue){
+            currValue.activated = true;
+            return currValue;
+        });
+    }
+    
     
     return CircularHitRegions;
 });
